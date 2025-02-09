9ead258aedbd65760297eae50c5bc0b68d9bc914
Inji Mammadli <mammi910@newschool.edu>
2025-02-09 18:24:12 -0500

    Added arena

--- arena.js
+++ arena.js
@@ -1,11 +1,41 @@
 // Okay, Are.na stuff!
-let channelSlug = 'typography-and-interaction-too' // The “slug” is just the end of the URL
+let channelSlug = 'risograph-5nwec8aul3u' // The “slug” is just the end of the URL
 
 // This allows us to process/render the descriptions, which are in Markdown!
 // More about Markdown: https://en.wikipedia.org/wiki/Markdown
 let markdownIt = document.createElement('script')
 markdownIt.src = 'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
 document.head.appendChild(markdownIt)
+
+
+
+let renderBlock = (block) => {
+	console.log(block);
+  
+	let blockContainer = document.querySelector("#channel-blocks");
+  
+	let linkItem;
+  
+	console.log(block.image)
+  
+	if (block.image) {
+	  linkItem = `
+		<picture>
+		  <source media="(max-width: 428px)" srcset="${block.image.thumb.url}">
+		  <source media="(max-width: 640px)" srcset="${block.image.large.url}">
+		  <img src="${block.image.original.url}">
+		</picture>`;
+	} else {
+	  linkItem = `
+		<span>
+		  <h3>${block.title}</h3>
+		</span>`;
+	}
+  
+	blockContainer.insertAdjacentHTML("beforeend", linkItem);
+  };
+  
+
 
 
 
@@ -27,5 +57,7 @@ fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-stor
 		console.log(data) // Always good to check your response!
 		console.log (data.title)
 		console.log(data.created_art)
-		
+		data.contents.forEach(block => {
+			renderBlock(block)
+		});
 	})
