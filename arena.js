0c994585f5535972d77def2c042a00c2302e7d68
Inji Mammadli <mammi910@newschool.edu>
2025-02-18 21:37:35 -0500

    adding text, attachment and link modals

--- arena.js
+++ arena.js
@@ -52,7 +52,7 @@ fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-stor
 		// 	renderBlock(block)
 		// });
 
-		console.log (blocks)
+		console.log(blocks)
 	})
 
 // printing
@@ -71,13 +71,16 @@ let printRandomBlock = () => {
 	if (paper.innerHTML.trim()) timeout = 1000;
 	setTimeout(() => {
 
-		// get random block that has an image
-		let blocksWithImage = blocks.filter((block) => block.image);
-		let randomBlock = blocksWithImage[Math.floor(Math.random() * blocksWithImage.length)];
+		// let randomBlock = blocks[3];
+		let randomBlock = blocks[Math.floor(Math.random() * blocks.length)];
 
+		if (randomBlock.image) {
+			paper.innerHTML = `<img src="${randomBlock.image.original.url}" />`;
+		}
 
-		//initiate printer
-		paper.innerHTML = `<img src="${randomBlock.image.original.url}" />`;
+		else {
+			paper.innerHTML = `<div class="text-block"><p>Read:</p><h3>${randomBlock.title}</h3></div>`
+		}
 		paper.onclick = () => {
 			insertBlockintoModal(randomBlock);
 		}
@@ -101,7 +104,7 @@ let insertBlockintoModal = (block) => {
 	showModal();
 	let modalContent = document.querySelector("#modal-content");
 	if (block.class === "Media") {
-		modalContent.innerHTML = `<div class="modal-video"><h3>${block.title}</h3> ${block.embed.html}	<p id="modal-text"> ${block.description ||""} </p> <a class= "button"> view original <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
+		modalContent.innerHTML = `<div class="modal-video"><h3>${block.title}</h3> ${block.embed.html}	<p id="modal-text"> ${block.description || ""} </p> <a class= "button"> view on arena <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
 						<g clip-path="url(#clip0_82_41092)">
 							<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
 						</g>
@@ -114,11 +117,60 @@ let insertBlockintoModal = (block) => {
 	</div>`;
 	}
 
+	else if (block.class === "Text") {
+		modalContent.innerHTML = `<div class="modal-image">  <h3>${block.title}</h3> <p id="modal-p">${block.content}</p>
+			
+			</p><a class="button"> view original <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
+				<g clip-path="url(#clip0_82_41092)">
+					<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+				</g>
+				<defs>
+					<clipPath id="clip0_82_41092">
+						<rect width="50" height="100" fill="white" transform="matrix(0 1 -1 0 100 0)" />
+					</clipPath>
+				</defs>
+			</svg> </a>
+		</div>`;
+	}
+
+	else if (block.class === "Link") {
+		modalContent.innerHTML = `<div class="modal-image">  <h3>${block.title}</h3>
+			<p id="modal-text"> ${block.description || ""}
+	</p><img src="${block.image.original.url}" />
+			</p><a class="button"> go to website <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
+				<g clip-path="url(#clip0_82_41092)">
+					<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+				</g>
+				<defs>
+					<clipPath id="clip0_82_41092">
+						<rect width="50" height="100" fill="white" transform="matrix(0 1 -1 0 100 0)" />
+					</clipPath>
+				</defs>
+			</svg> </a>
+		</div>`;
+	}
+
+	else if (block.class === "Attachment") {
+		modalContent.innerHTML = `<div class="modal-image">  <h3>${block.title}</h3>
+			<p id="modal-text"> ${block.description || ""}
+	</p><img src="${block.image.original.url}" />
+			</p><a class="button"> see full pdf <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
+				<g clip-path="url(#clip0_82_41092)">
+					<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+				</g>
+				<defs>
+					<clipPath id="clip0_82_41092">
+						<rect width="50" height="100" fill="white" transform="matrix(0 1 -1 0 100 0)" />
+					</clipPath>
+				</defs>
+			</svg> </a>
+		</div>`;
+	}
 
 	else {
-		modalContent.innerHTML = `<div class="modal-image"><h3>${block.title}</h3>
-	<img src="${block.image.original.url}" />
-	<p id="modal-text"> ${block.description ||""} </p> <a class= "button"> view original <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
+		modalContent.innerHTML = `<div class="modal-image" >  <h3>${block.title}</h3><img src="${block.image.original.url}" />
+	 <p id="modal-text"> ${block.description || ""}
+	</p><a class= "button"> view original <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
 						<g clip-path="url(#clip0_82_41092)">
 							<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
 						</g>
@@ -128,6 +180,6 @@ let insertBlockintoModal = (block) => {
 							</clipPath>
 						</defs>
 					</svg> </a>
-	</div>`;
+	</div> `;
 	}
 }
