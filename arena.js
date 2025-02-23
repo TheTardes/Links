ab3e3dc1d9acf1cdcbce6de5a6ba452a53d63df8
Inji Mammadli <mammi910@newschool.edu>
2025-02-22 21:15:40 -0500

    fixing link between arena and content

--- arena.js
+++ arena.js
@@ -105,7 +105,12 @@ let insertBlockintoModal = (block) => {
 	showModal();
 	let modalContent = document.querySelector("#modal-content");
 	if (block.class === "Media") {
-		modalContent.innerHTML = `<div class="modal-video"><h3>${block.title}</h3> ${block.embed.html}	<p id="modal-text"> ${block.description || ""} </p> <a class= "button"> view on arena <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
+		console.log(block.description)
+		console.log(block)
+		modalContent.innerHTML = `<div class="modal-video">
+		<h3>${block.title}</h3> ${block.embed.html}
+		<p  class="margin" id="modal-text"> ${block.description || ""} </p> 
+		<a class= "button"> view on arena <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
 						<g clip-path="url(#clip0_82_41092)">
 							<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
 						</g>
@@ -119,9 +124,10 @@ let insertBlockintoModal = (block) => {
 	}
 
 	else if (block.class === "Text") {
-		modalContent.innerHTML = `<div class="modal-image">  <h3>${block.title}</h3> <p id="modal-p">${block.content}</p>
-			
-			</p><a class="button"> view original <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
+		modalContent.innerHTML = `<div class="modal-image">
+		<h3>${block.title}</h3>
+		<p  class="margin" id="modal-p">${block.content}</p>
+		<a href="https://www.are.na/block/${block.id}" class="button"> view original <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
 				<g clip-path="url(#clip0_82_41092)">
 					<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
 				</g>
@@ -130,34 +136,34 @@ let insertBlockintoModal = (block) => {
 						<rect width="50" height="100" fill="white" transform="matrix(0 1 -1 0 100 0)" />
 					</clipPath>
 				</defs>
-			</svg> </a>
+			</svg></a>
 		</div>`;
 	}
 
 	else if (block.class === "Link") {
-		modalContent.innerHTML = `<div class="modal-image">  <h3>${block.title}</h3>
-			<p id="modal-text"> ${block.description || ""}
-	</p><img src="${block.image.original.url}" />
-			</p><a class="button"> go to website <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
+		modalContent.innerHTML = `<div class="modal-image">
+		<h3>${block.title}</h3>
+		<p id="modal-text"> ${block.description || ""}</p>
+		<img class="margin" src="${block.image.original.url}" />
+		<a href="https://www.are.na/block/${block.id}" class="button"> go to website <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
 				<g clip-path="url(#clip0_82_41092)">
-					<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-				</g>
+					<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" /></g>
 				<defs>
 					<clipPath id="clip0_82_41092">
 						<rect width="50" height="100" fill="white" transform="matrix(0 1 -1 0 100 0)" />
 					</clipPath>
 				</defs>
-			</svg> </a>
+			</svg></a>
 		</div>`;
 	}
 
 	else if (block.class === "Attachment") {
-		modalContent.innerHTML = `<div class="modal-image">  <h3>${block.title}</h3>
-			<p id="modal-text"> ${block.description || ""}
-	</p><img src="${block.image.original.url}" />
-			</p><a class="button"> see full pdf <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
-				<g clip-path="url(#clip0_82_41092)">
-					<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+		modalContent.innerHTML = `<div class="modal-image">
+		<h3>${block.title}</h3>
+		<img class="margin" src="${block.image.original.url}" />
+		<p id="modal-text"> ${block.description || ""}</p>
+		<a href="https://www.are.na/block/${block.id}" class="button"> see full pdf <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
+			<g clip-path="url(#clip0_82_41092)"><path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
 				</g>
 				<defs>
 					<clipPath id="clip0_82_41092">
@@ -169,9 +175,11 @@ let insertBlockintoModal = (block) => {
 	}
 
 	else {
-		modalContent.innerHTML = `<div class="modal-image" >  <h3>${block.title}</h3><img src="${block.image.original.url}" />
-	 <p id="modal-text"> ${block.description || ""}
-	</p><a class= "button"> view original <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
+		modalContent.innerHTML = `<div class="modal-image" >
+		<h3>${block.title}</h3>
+		<img src="${block.image.original.url}" />
+		<p class="margin" id="modal-text"> ${block.description || ""}</p>
+		<a href="https://www.are.na/block/${block.id}" class= "button"> view original <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50" fill="none">
 						<g clip-path="url(#clip0_82_41092)">
 							<path d="M32.5 23.0002C54.9525 26.9752 56.7376 24.1277 79.4001 25.5977M69.0826 42.0077C75.4851 33.1027 86.2576 32.2277 93.9326 25.0702C86.5476 21.9752 71.8601 15.0127 67.1951 7.99268" stroke="black" stroke-width="4" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
 						</g>
@@ -181,50 +189,50 @@ let insertBlockintoModal = (block) => {
 							</clipPath>
 						</defs>
 					</svg> </a>
-	</div> `;
+			</div>`;
 	}
 }
 
 // list
 let insertBlocks = () => {
 	let list1 = document.querySelector(".list-1");
-	list1.children[1].innerHTML = `<picture><img src="${blocks[0].image.original.url}" /></picture>
-									<p class="text-block"> 
-									Read on: ${blocks[3].title}</p>
-									<picture><img src="${blocks[5].image.original.url}" /></picture>
-									<picture><img src="${blocks[41].image.original.url}" /></picture>`;
+	list1.children[1].innerHTML = `<a href="https://www.are.na/block/${blocks[0].id}" ><img src="${blocks[0].image.original.url}"/></a>
+									<a href="https://www.are.na/block/${blocks[3].id}"><p class="text-media"> 
+									Read: ${blocks[3].title}</p></a>
+									<a href="https://www.are.na/block/${blocks[5].id}"><img src="${blocks[5].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[41].id}"><img src="${blocks[41].image.original.url}" /></a>`;
 	let list2 = document.querySelector(".list-2");
-	list2.children[1].innerHTML = `<picture><img src="${blocks[1].image.original.url}" /></picture>
-									<picture><img src="${blocks[2].image.original.url}" /></picture>
-									<picture><img src="${blocks[8].image.original.url}" /></picture>
-									<picture><img src="${blocks[11].image.original.url}" /></picture>
-									<picture><img src="${blocks[23].image.original.url}" /></picture>
-									<picture><img src="${blocks[24].image.original.url}" /></picture>
-									<picture><img src="${blocks[25].image.original.url}" /></picture>
-									<picture><img src="${blocks[30].image.original.url}" /></picture>
-									<picture><img src="${blocks[34].image.original.url}" /></picture>
-									<picture><img src="${blocks[36].image.original.url}" /></picture>
-									<picture><img src="${blocks[37].image.original.url}" /></picture>`;
+	list2.children[1].innerHTML = `<a href="https://www.are.na/block/${blocks[1].id}"><img src="${blocks[1].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[2].id}"><img src="${blocks[2].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[8].id}"><img src="${blocks[8].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[11].id}"><img src="${blocks[11].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[23].id}"><img src="${blocks[23].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[24].id}"><img src="${blocks[24].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[25].id}"><img src="${blocks[25].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[30].id}"><img src="${blocks[30].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[34].id}"><img src="${blocks[34].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[36].id}"><img src="${blocks[36].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[37].id}"><img src="${blocks[37].image.original.url}" /></a>`;
 	let list3 = document.querySelector(".list-3");
-	list3.children[1].innerHTML = `<picture><img src="${blocks[13].image.original.url}" /></picture>
-									<p class="text-block"> 
-									Read on: ${blocks[14].title}</p>
-									<picture><img src="${blocks[15].image.original.url}" /></picture>
-									<picture><img src="${blocks[17].image.original.url}" /></picture>
-									<p class="text-block"> 
-									Read on: ${blocks[19].title}</p>
-									<picture><img src="${blocks[21].image.original.url}" /></picture>
-									<picture><img src="${blocks[28].image.original.url}" /></picture>`;
+	list3.children[1].innerHTML = `<a href="https://www.are.na/block/${blocks[13].id}"><img src="${blocks[13].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[14].id}"><p class="text-media"> 
+									Read: ${blocks[14].title}</p></a>
+									<a href="https://www.are.na/block/${blocks[15].id}"><img src="${blocks[15].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[17].id}"><img src="${blocks[17].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[17].id}"><p class="text-media"> 
+									Read: ${blocks[19].title}</p></a>
+									<a href="https://www.are.na/block/${blocks[21].id}"><img src="${blocks[21].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[28].id}"><img src="${blocks[28].image.original.url}" /></a>`;
 	let list4 = document.querySelector(".list-4");
-	list4.children[1].innerHTML = `<picture><img src="${blocks[4].image.original.url}" /></picture>
-									<picture><img src="${blocks[9].image.original.url}" /></picture>
-									<picture><img src="${blocks[12].image.original.url}" /></picture>
-									<picture><img src="${blocks[16].image.original.url}" /></picture>
-									<picture><img src="${blocks[18].image.original.url}" /></picture>
-									<picture><img src="${blocks[22].image.original.url}" /></picture>
-									<picture><img src="${blocks[27].image.original.url}" /></picture>
-									<picture><img src="${blocks[33].image.original.url}" /></picture>
-									<picture><img src="${blocks[38].image.original.url}" /></picture>
+	list4.children[1].innerHTML = `<a href="https://www.are.na/block/${blocks[4].id}"><img src="${blocks[4].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[9].id}"><img src="${blocks[9].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[12].id}"><img src="${blocks[12].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[16].id}"><img src="${blocks[16].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[18].id}"><img src="${blocks[18].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[22].id}"><img src="${blocks[22].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[27].id}"><img src="${blocks[27].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[33].id}"><img src="${blocks[33].image.original.url}" /></a>
+									<a href="https://www.are.na/block/${blocks[38].id}"><img src="${blocks[38].image.original.url}" /></a>
 									`;
 };
 
